7160e63b7fb60b68ae10334765c05f4636193f6b
AKlychkova <adklychkova@edu.hse.ru>
2025-03-15 19:36:43 +0300

    feat: add colors to bottom navigation bar

--- app/src/main/java/ru/hse/crowns/MainActivity.kt
+++ app/src/main/java/ru/hse/crowns/MainActivity.kt
@@ -1,6 +1,7 @@
 package ru.hse.crowns
 
 import android.os.Bundle
+import android.view.MenuItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -21,7 +22,34 @@ class MainActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Set color for selected tab in navigation bar
+        navView.setOnItemSelectedListener { item: MenuItem ->
+            when (item.itemId) {
+                R.id.n_queens_tab -> {
+                    navView.itemIconTintList =
+                        resources.getColorStateList(R.color.bottom_nav_n_queens, null)
+                    navView.itemTextColor =
+                        resources.getColorStateList(R.color.bottom_nav_n_queens, null)
+                }
+
+                R.id.queens_tab -> {
+                    navView.itemIconTintList =
+                        resources.getColorStateList(R.color.bottom_nav_queens, null)
+                    navView.itemTextColor =
+                        resources.getColorStateList(R.color.bottom_nav_queens, null)
+                }
+
+                R.id.killer_sudoku_tab -> {
+                    navView.itemIconTintList =
+                        resources.getColorStateList(R.color.bottom_nav_killer_sudoku, null)
+                    navView.itemTextColor =
+                        resources.getColorStateList(R.color.bottom_nav_killer_sudoku, null)
+                }
+            }
+            true
+        }
+
+        val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
