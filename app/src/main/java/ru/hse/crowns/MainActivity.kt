486a8c9f5aa3609d35b3972d2cb46afdb4c32e86
AKlychkova <adklychkova@edu.hse.ru>
2025-03-16 19:06:16 +0300

    fix: fix color change and navigation collision

--- app/src/main/java/ru/hse/crowns/MainActivity.kt
+++ app/src/main/java/ru/hse/crowns/MainActivity.kt
@@ -1,12 +1,8 @@
 package ru.hse.crowns
 
 import android.os.Bundle
-import android.view.MenuItem
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import ru.hse.crowns.databinding.ActivityMainBinding
 
@@ -20,44 +16,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        // Set up navigation between home screens
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.navView.setupWithNavController(navController)
 
-        // Set color for selected tab in navigation bar
-        navView.setOnItemSelectedListener { item: MenuItem ->
-            when (item.itemId) {
-                R.id.n_queens_tab -> {
-                    navView.itemIconTintList =
-                        resources.getColorStateList(R.color.bottom_nav_n_queens, null)
-                    navView.itemTextColor =
-                        resources.getColorStateList(R.color.bottom_nav_n_queens, null)
-                }
-
-                R.id.queens_tab -> {
-                    navView.itemIconTintList =
-                        resources.getColorStateList(R.color.bottom_nav_queens, null)
-                    navView.itemTextColor =
-                        resources.getColorStateList(R.color.bottom_nav_queens, null)
-                }
-
-                R.id.killer_sudoku_tab -> {
-                    navView.itemIconTintList =
-                        resources.getColorStateList(R.color.bottom_nav_killer_sudoku, null)
-                    navView.itemTextColor =
-                        resources.getColorStateList(R.color.bottom_nav_killer_sudoku, null)
-                }
-            }
-            true
-        }
-
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        // Set icon tint list to null to use original colors of menuItems
+        binding.navView.itemIconTintList = null
     }
 }
